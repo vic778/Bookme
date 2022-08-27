c28fa7a477710f234263fa5a250848e1f8ecfe8e
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-27 14:44:05 +0300

    Redirect user to the dashbaord after signup

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
 
 
   devise_for :users
-  root to: 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  authenticated :user do
+    root 'home#dashboard', as: :authenticated_root
+  end
+  root to: 'home#index'
+ 
 end
