4df34844cad3e02a2c1d6a784e4be4fcb95d966c
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-28 15:20:44 +0300

    Generate booking model

---
+++ db/migrate/20220827114719_create_bookings.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class CreateBookings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookings do |t|
+      t.integer :status, default: 0, null: false
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.datetime :start_at
+      t.datetime :end_at
+      t.boolean :customer_paid, default: false
+
+      t.timestamps
+    end
+  end
+end
