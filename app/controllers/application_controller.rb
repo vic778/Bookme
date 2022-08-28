4b466cb5d9e1cae9d2f76efb061f99d3ebce50ab
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-28 15:10:07 +0300

    Fix linters errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
 
   protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :booking_link])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :booking_link])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name booking_link])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name booking_link])
+  end
 end
