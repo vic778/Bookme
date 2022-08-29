ecfa076e9830e9be0e718cdac27073e20ab1e3bb
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-29 18:47:38 +0300

    Find a user via the booking_link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
     # @user = find_by(booking_link: params[:booking_link])
     @user = User.find_by(booking_link: params[:booking_link])
-    @user = User.find_by(name: params[:name]) if params[:name]
-  
+
   end
 end
