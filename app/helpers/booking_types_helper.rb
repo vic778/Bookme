ce2485d3af03e5c8fa63d963ecf17b6c7386377c
vic778 <victoiremmanuelbarh@gmail.com>
2022-08-29 18:48:05 +0300

    create bookingtype methode to render the min and hr

--- app/helpers/booking_types_helper.rb
+++ app/helpers/booking_types_helper.rb
@@ -1,2 +1,9 @@
 module BookingTypesHelper
+    def duration(type)
+        if type.duration == 60
+            return "1 hr"
+        else
+            type.duration.to_s + " mins"
+        end
+    end
 end
